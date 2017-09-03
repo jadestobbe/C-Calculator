e17a5efa7774ea7b3f4c118145e3a63808a69741
Jade Stobbe <jadestobbe@gmail.com>
2017-09-03 11:38:22 -0500

    created calculator.h

---
+++ calculator.h
@@ -0,0 +1,11 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+class Calculator{
+  float add(float, float);
+  float subtract(float, float);
+  float multiply(float, float);
+  float divide(float, float);
+  float exponent(float, int);
+  int factorial(int);
+};
+#endif //CALCULATOR_H
